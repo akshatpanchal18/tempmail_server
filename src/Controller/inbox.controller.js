55aed3ecd4e061e3ce7eff49a5f38ba957e1e721
Akshat Panchal <akshatpanchal001@gmail.com>
2024-12-11 14:51:41 +0530

    inbox controller change

--- src/Controller/inbox.controller.js
+++ src/Controller/inbox.controller.js
@@ -6,14 +6,16 @@ import mongoose, { isValidObjectId } from "mongoose";
 import { Tempmail } from "../Models/email.models.js";
 
 const addMessageToInbox = asyncHandler(async (req, res) => {
-    const { recipient, sender, "subject":sub, 'body-plain': body } = req.body['event-data'];
+    const { recipient, sender, "subject":sub, 'body-plain': body } = req.body;
     // const {mailid, text, from, subject } = req.body;
     // const mailid = req.params.mailid.trim(); // Get mailid from route params
     const findEmail = await Tempmail.find({"tempEmail":recipient})
-    const mailid = findEmail._id;
+    // const mailid = findEmail.inbox;
+    const mailid = findEmail.length > 0 ? findEmail[0]._id : null
     const from = sender;
     const subject = sub;
     const text = body;
+console.log(mailid);
 
     console.log(`Received email from ${from} to ${recipient} with subject: ${subject}`);
 
